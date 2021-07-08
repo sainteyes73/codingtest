4aba02c21bf4b54726739c19ebd6c34f3c6c1970
Ji-sunny <2jisuning@gmail.com>
2021-07-08 16:45:07 +0900

    1주차 - 이지선 로봇청소기 통과

--- week01/이지선/cleaner.java
+++ week01/이지선/cleaner.java
@@ -3,14 +3,15 @@ import java.io.InputStreamReader;
 
 public class cleaner {
     static int[][] map;
-    static int move[][] = {{0,-1}, {1, 0}, {0,1}, {-1,0}};
-    static int back[][] = {{1,0}, {0, 1}, {-1,0}, {0,-1}};
+    static int move[][] = {{-1,0}, {0, 1}, {1,0}, {0,-1}}; //북 동 남 서
     static int answer = 0;
+    static int N;
+    static int M;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] str = br.readLine().split(" ");
-        int N = Integer.parseInt(str[0]);
-        int M = Integer.parseInt(str[1]);
+        N = Integer.parseInt(str[0]);
+        M = Integer.parseInt(str[1]);
 //        System.out.println("N"+N +"M" +M);
         String[] str2 = br.readLine().split(" ");
         int r = Integer.parseInt(str2[0]);
@@ -20,48 +21,46 @@ public class cleaner {
         map = new int[N][M];
         for (int i=0; i<N; i++) {
             String[] str3 = br.readLine().split(" ");
-
             for(int j=0; j<M; j++) {
-//                System.out.println(str3[j]);
                 map[i][j] = Integer.parseInt(str3[j]);
 
             }
         }
         clean(r, c, d);
         System.out.println(answer);
-        for (int i=0; i<N; i++) {
-            for(int j=0; j<M; j++) {
-                System.out.print(map[i][j]);
-            }
-            System.out.println();
-        }
+
     }
     static void clean (int row, int col, int direct) {
         //청소를 한경우 0에서 2로 변경
+
         if(map[row][col] == 0) {
             map[row][col] =2;
             answer ++;
         }
 
         for( int i=0; i<4; i++){
-            if(check(row, col, direct)) {
-                row = row + move[direct][0];
-                col = col + move[direct][1];
+            if(check(row, col, turn(direct))) {
+
+                row = row + move[turn(direct)][0];
+                col = col + move[turn(direct)][1];
                 clean(row, col, turn(direct));
                 return;
             }
             direct = turn(direct);
         }
+
         if(back(row, col, direct)) {
-            row = row + back[direct][0];
-            col = col + back[direct][1];
+            int backdirect = (direct + 2) % 4;
+            row = row + move[backdirect][0];
+            col = col + move[backdirect][1];
             clean(row, col, direct);
         } else return;
     }
-
+    // 북이면 남쪽으로 0이면 2로
     static boolean back(int row, int col, int direct) {
-        row = row + back[direct][0];
-        col = col + back[direct][1];
+        direct = (direct + 2) % 4;
+        row = row + move[direct][0];
+        col = col + move[direct][1];
         return map[row][col] != 1;
     }
 
@@ -70,11 +69,34 @@ public class cleaner {
         col = col + move[direct][1];
         return map[row][col] == 0;
     }
-
+    //북 >> 서 0이면 3
     static Integer turn (int arrow){
-        arrow = arrow + 1;
-        if(arrow>3) return 0;
+        arrow = (arrow + 3) %4;
         return arrow;
     }
 }
+/*
+9 7
+6 2 1
+  0 1 2 3 4 5 6
+0 1 1 1 1 1 1 1
+1 1 2 1 0 1 0 1
+2 1 2 1 0 0 0 1
+3 1 2 1 0 1 0 1
+4 1 2 1 1 1 1 1
+5 1 2 2 2 2 2 1
+6 1 2 2 1 1 2 1
+7 1 2 2 2 2 2 1
+8 1 1 1 1 1 1 1
+>> 13
+6 6
+2 1 3
+1 1 1 1 1 1
+1 0 0 0 0 1
+1 0 1 1 1 1
+1 0 1 1 1 1
+1 0 1 1 1 1
+1 1 1 1 1 1
+>>7
 
+ */
